ef73564f32998cd29899e5449dbe51e4c1250b87
luwanx <luwanxi@neusoft.com>
2023-07-17 16:23:31 +0800

    aa

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-
-
-#
-zip -r ${CI_TAG}.zip $CI_DERIVED_DATA_PATH
-
-
+#压缩文件夹
+zip -r ${CI_TAG}.zip ${CI_DERIVED_DATA_PATH}
 
 # 获取文件夹的上级路径
 parent_path=$(dirname "${CI_DERIVED_DATA_PATH}")
@@ -16,28 +12,32 @@ zip_file=${parent_path}"/"${CI_TAG}".zip"
 # 压缩文件夹为 ZIP 格式
 zip -r "${zip_file}" "${CI_DERIVED_DATA_PATH}"
 
-
 # GitHub 认证信息
 github_token="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"  # 替换为你的 GitHub Personal Access Token
 github_owner="luwanx"  # 替换为 GitHub 仓库的所有者用户名
 github_repo="neusoftBSDxcodeCloudTest"  # 替换为 GitHub 仓库的名称
 
-# 创建 GitHub Release
-release_name="Release"${CI_TAG}  # 替换为你想要的发布名称
+# Release 信息
+release_tag=${CI_TAG}  # 替换为新 Release 的标签
+release_name="Release"${CI_TAG}  # 替换为新 Release 的名称
+release_body="Release notes"${CI_TAG}  # 替换为新 Release 的说明文本
+file_name=${CI_TAG}".zip"  # 替换为要添加的文件的名称
+
+# 创建新 Release
 release_url="https://api.github.com/repos/${github_owner}/${github_repo}/releases"
 headers=(
   "-H" "Authorization: Bearer ${github_token}"
   "-H" "Accept: application/vnd.github.v3+json"
 )
 data=(
-  "-d" "{\"tag_name\": \"${CI_TAG}\", \"name\": \"${release_name}\", \"draft\": false, \"prerelease\": false}"
+  "-d" "{\"tag_name\": \"${release_tag}\", \"name\": \"${release_name}\", \"body\": \"${release_body}\"}"
 )
-response=$(curl -X POST "${headers[@]}" "${data[@]}" "${release_url}")
+response=$(curl -s "${headers[@]}" "${data[@]}" "${release_url}")
 release_id=$(echo "${response}" | grep -Po '"id": \K[^,]*')
 
-# 上传 IPA 文件到 GitHub Release
-upload_url="https://uploads.github.com/repos/${github_owner}/${github_repo}/releases/${release_id}/assets?name=${zip_file}"
+# 上传文件到 Release
+upload_url="https://uploads.github.com/repos/${github_owner}/${github_repo}/releases/${release_id}/assets?name=${file_name}"
 headers+=("-H" "Content-Type: application/octet-stream")
-response=$(curl -X POST "${headers[@]}" --data-binary "@${zip_file}" "${upload_url}")
+response=$(curl -s -X POST "${headers[@]}" --data-binary "@${zip_file}" "${upload_url}")
 
-echo "IPA 文件上传成功！"
+echo "文件已添加到 GitHub Release！"
