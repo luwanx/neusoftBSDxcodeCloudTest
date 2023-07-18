133c49e81ee489bd74c3d2d4447a1beb162e8a4e
luwanx <luwanxi@neusoft.com>
2023-07-18 15:43:55 +0800

    test scripts 4

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,23 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 # 原始文件夹路径和标签名称
 #CI_DERIVED_DATA_PATH="/Users/biprogybank06/Desktop/neusoftBSDxcodeCloudTest 2023-07-17 14-23-19"
-#CI_TAG="v0.0.7"
-
-# 压缩文件保存路径和名称
-zip_file="${CI_DERIVED_DATA_PATH}/${CI_TAG}.zip"
-
-# 压缩文件夹为 ZIP 格式
-zip -r "${zip_file}" "$(basename "${CI_DERIVED_DATA_PATH}")"
-
-echo "文件夹已成功压缩为 ZIP 文件！"
-
-if [ -e "$zip_file" ]; then
-    fileZipSuccess="文件存在"
-else
-    fileZipSuccess="文件不存在"
-fi
-
+#CI_TAG="v0.0.6"
 
 # GitHub 认证信息
 github_t="ghp_"
@@ -33,8 +18,7 @@ github_repo="neusoftBSDxcodeCloudTest"  # 替换为 GitHub 仓库的名称
 # Release 信息
 release_tag=${CI_TAG}  # 替换为新 Release 的标签
 release_name="Release${CI_TAG}"  # 替换为新 Release 的名称
-release_body="Release notes：${CI_TAG}，CI_DERIVED_DATA_PATH=${CI_DERIVED_DATA_PATH},文件是否存在：${fileZipSuccess}"  # 替换为新 Release 的说明文本
-file_name=${CI_TAG}.zip  # 替换为要添加的文件的名称
+release_body="Release notes：${CI_TAG}，CI_DERIVED_DATA_PATH=${CI_DERIVED_DATA_PATH}"  # 替换为新 Release 的说明文本
 
 # 创建新 Release
 release_url="https://api.github.com/repos/${github_owner}/${github_repo}/releases"
@@ -48,13 +32,21 @@ release_id=$(echo "${response}" | jq -r '.id')
 
 echo "查看 release_id：${release_id}"
 
-# 上传文件到 Release
-upload_url="https://uploads.github.com/repos/${github_owner}/${github_repo}/releases/${release_id}/assets?name=${file_name}"
-headers+=("-H" "Content-Type: application/zip")
-response=$(curl -i -X POST "${headers[@]}" --data-binary "@${zip_file}" "${upload_url}")
+# 获取文件列表
+file_array=( "${CI_DERIVED_DATA_PATH}"/* )
 
-echo "查看 response：${response}"
-
-echo "查看 upload_url：${upload_url}"
-
-echo "文件已添加到 GitHub Release！"
+# 打印所有文件路径
+for file in "${file_array[@]}"; do
+    if [[ -f "$file" ]]; then
+        mime_type=$(file -b --mime-type "$file")
+        
+        # 上传文件到 Release
+        upload_url="https://uploads.github.com/repos/${github_owner}/${github_repo}/releases/${release_id}/assets?name=$(basename "$file")"
+        headers+=("-H" "Content-Type: ${mime_type}")
+        response=$(curl -i -X POST "${headers[@]}" --data-binary "@${file}" "${upload_url}")
+        
+        echo "查看 response：${response}"
+        echo "查看 upload_url：${upload_url}"
+        echo "${file} 文件已添加到 GitHub Release！"
+    fi
+done
