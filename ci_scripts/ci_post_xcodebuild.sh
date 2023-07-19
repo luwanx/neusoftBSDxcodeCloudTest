2cef4ae342bfc2dc0caabd0b41f0f89ad1c7f6c9
luwanx <luwanxi@neusoft.com>
2023-07-19 10:42:47 +0800

    test scripts 7.19-1

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -3,9 +3,10 @@
 
 brew install jq
 
-# 原始文件夹路径和标签名称
-#CI_DERIVED_DATA_PATH="/Users/biprogybank06/Desktop/neusoftBSDxcodeCloudTest 2023-07-17 14-23-19"
-#CI_TAG="v0.0.7"
+#CI_TAG="v0.0.12"
+# 定义数组
+path_array=("$CI_AD_HOC_SIGNED_APP_PATH" "$CI_APP_STORE_SIGNED_APP_PATH" "$CI_DEVELOPMENT_SIGNED_APP_PATH" "$CI_DEVELOPER_ID_SIGNED_APP_PATH")
+#path_array=("/Users/biprogybank06/Desktop/test1" "/Users/biprogybank06/Desktop/test2")
 
 # GitHub 认证信息
 github_t="ghp_"
@@ -14,25 +15,81 @@ github_k="sgeBKDD"
 github_e="zBL3bfU4nC"
 github_n="5Zia0guwW0NVd0K"
 github_tt="${github_t}${github_o}${github_k}${github_e}${github_n}"
+
 echo "查看 github_tt：${github_tt}"
+
 github_owner="luwanx"  # 替换为 GitHub 仓库的所有者用户名
 github_repo="neusoftBSDxcodeCloudTest"  # 替换为 GitHub 仓库的名称
 
 # Release 信息
 release_tag=${CI_TAG}  # 替换为新 Release 的标签
 release_name="Release${CI_TAG}"  # 替换为新 Release 的名称
-release_body="Release notes：${CI_TAG}，CI_ARCHIVE_PATH=${CI_ARCHIVE_PATH}"  # 替换为新 Release 的说明文本
+
+release_body="Release notes：${CI_TAG}"  # 替换为新 Release 的说明文本
+release_id=""
+
+#获取路径下所有文件名称
+getFileNameInPath() {
+    local path=$1
+    local body="$(basename "$path"):"
+    # 获取文件列表
+    local file_array=( "${path}"/* )
+    # 打印所有文件路径
+    for file in "${file_array[@]}"; do
+        if [[ -f "$file" ]]; then
+            body="${body},$(basename "$file")"
+        fi
+    done
+    release_body="${release_body}  -------  ${body}"
+}
 
 
-## 获取文件列表
-#file_array=( "${CI_ARCHIVE_PATH}"/* )
-## 打印所有文件路径
-#for file in "${file_array[@]}"; do
-#    if [[ -f "$file" ]]; then
-#        release_body="${release_body},$(basename "$file")"
-#    fi
-#done
 
+#上传路径中所有文件
+postPathAllFile(){
+    local path=$1
+    
+    local headers=(
+        "-H" "Authorization: Bearer ${github_tt}"
+        "-H" "Accept: application/vnd.github.v3+json"
+    )
+    
+    local pathName="$(basename "$path"):"
+    
+    # 获取文件列表
+    local file_array=( "${path}"/* )
+
+    # 打印所有文件路径
+    for file in "${file_array[@]}"; do
+        if [[ -f "$file" ]]; then
+
+            local mime_type=$(file -b --mime-type "$file")
+
+            #文件名
+            local fileName="${pathName}-$(basename "$file")"
+            echo "查看 fileName：${fileName}"
+            
+            # 上传文件到 Release
+            local upload_url="https://uploads.github.com/repos/${github_owner}/${github_repo}/releases/${release_id}/assets?name=${fileName}"
+            headers+=("-H" "Content-Type: ${mime_type}")
+            local response=$(curl -i -X POST "${headers[@]}" --data-binary "@${file}" "${upload_url}")
+
+            echo "查看 response：${response}"
+            echo "查看 upload_url：${upload_url}"
+            echo "${file} 文件已添加到 GitHub Release！"
+
+        fi
+    done
+}
+
+
+
+
+# 遍历数组
+for path in "${path_array[@]}"; do
+    #调用函数，拼接body
+    getFileNameInPath "$path"
+done
 
 
 # 创建新 Release
@@ -47,36 +104,14 @@ release_id=$(echo "${response}" | jq -r '.id')
 
 echo "查看 release_id：${release_id}"
 
-## 获取文件列表
-#file_array=( "${CI_ARCHIVE_PATH}"/* )
-#
-## 打印所有文件路径
-#for file in "${file_array[@]}"; do
-#    if [[ -f "$file" ]]; then
-#
-#        mime_type=$(file -b --mime-type "$file")
-#
-#        # 上传文件到 Release
-#        upload_url="https://uploads.github.com/repos/${github_owner}/${github_repo}/releases/${release_id}/assets?name=$(basename "$file")"
-#        headers+=("-H" "Content-Type: ${mime_type}")
-#        response=$(curl -i -X POST "${headers[@]}" --data-binary "@${file}" "${upload_url}")
-#
-#        echo "查看 response：${response}"
-#        echo "查看 upload_url：${upload_url}"
-#        echo "${file} 文件已添加到 GitHub Release！"
-#
-#    fi
-#done
+
+
+# 遍历数组
+for path in "${path_array[@]}"; do
+    #调用函数，上传文件
+    postPathAllFile "$path"
+done
 
 
 
-mime_type=$(file -b --mime-type "$CI_ARCHIVE_PATH")
-        
-# 上传文件到 Release
-upload_url="https://uploads.github.com/repos/${github_owner}/${github_repo}/releases/${release_id}/assets?name=$(basename "$CI_ARCHIVE_PATH")"
-headers+=("-H" "Content-Type: ${mime_type}")
-response=$(curl -i -X POST "${headers[@]}" --data-binary "@${CI_ARCHIVE_PATH}" "${upload_url}")
-        
-echo "查看 response：${response}"
-echo "查看 upload_url：${upload_url}"
-echo "${file} 文件已添加到 GitHub Release！"
+
